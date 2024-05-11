fd65b941e8de20e12221411859862ee567e75b1b
ThibautLra <lora.thibaut@gmail.com>
2024-05-11 12:03:58 +0200

    Initial Commit

---
+++ Expense-Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense-Tracker
+//
+//  Created by Thibaut Lora on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
